7c9bfcf5b38e780dd45881043fe3a5a09e147757
timothypartaliano <timothypartaliano@gmail.com>
2024-02-20 15:48:15 +0700

    integrate detail page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,19 @@ import { RouterModule } from '@angular/router';
     RouterModule,
   ],
   template: `
-  <main>
-    <a [routerLink]="['/']">
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-    </a>
-    <section class="content">
-      <router-outlet></router-outlet>
-    </section>
-  </main>
-`,
+    <main>
+      <a [routerLink]="['/']">
+        <header class="brand-name">
+          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+        </header>
+      </a>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'homes';
 }
-  
